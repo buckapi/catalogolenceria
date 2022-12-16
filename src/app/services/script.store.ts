5e3a6f716e20fd9ad4d10b183a12d293bb96beae
buckapi <buckapis@gmail.com>
2022-12-16 14:42:15 -0400

    first commit

---
+++ src/app/services/script.store.ts
@@ -0,0 +1,20 @@
+
+ interface Scripts {
+    name: string;
+    src: string;
+}  
+export const ScriptStore: Scripts[] = [
+
+  {name: 'jquery', src: '.assets/assetsCatalogo/js/vendor/jquery-3.2.1.min.js'},
+  {name: 'jquery-migrate', src: '.assets/assetsCatalogo/js/vendor/jquery-migrate.min.js'},
+  {name: 'skip', src: '.assets/assetsCatalogo/js/vendor/skip-link-focus-fix.js'},
+  {name: 'shortcodes', src: '.assets/assetsCatalogo/js/custom/shortcodes.js'},
+  {name: 'superfish', src: '.assets/assetsCatalogo/js/vendor/superfish.js'},
+  {name: '_main', src: '.assets/assetsCatalogo/js/custom/_main.js'},
+  {name: '_utils', src: '.assets/assetsCatalogo/js/custom/_utils.js'},
+  {name: '_init', src: '.assets/assetsCatalogo/js/custom/_init.js'},
+  {name: 'jquery.themepunch.tools', src: '.assets/assetsCatalogo/js/vendor/essential-grid/public/assets/js/jquery.themepunch.tools.min.js'},
+  {name: 'jquery.themepunch.revolution', src: '.assets/assetsCatalogo/js/vendor/revslider/public/assets/js/jquery.themepunch.revolution.min.js'},
+  {name: 'chosen.jquery', src: '.assets/assetsCatalogo/js/vendor/woocommerce-currency-switcher/js/chosen/chosen.jquery.min.js'},
+  {name: 'front', src: '.assets/assetsCatalogo/js/vendor/woocommerce-currency-switcher/js/front.js'}
+];
